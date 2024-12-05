f79b8ec11beab24d65ccbaf29ba5ce26a9a12394
PTARSONEVES <ptarsoneves@gmail.com>
2024-12-05 15:52:16 -0300

    configuracao de sessoes

--- db.js
+++ db.js
@@ -1,20 +1,62 @@
 require('dotenv').config();
-async function connect(query){
+
+// Conecta o Banco de Dados
+
+async function connect(){
     if(global.connection && global.connection.state !== 'disconnected')
         return global.connection;
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection(process.env.CONNECTION_MYSQL);
-/*
+//    const connection = await mysql.createConnection(process.env.CONNECTION_MYSQL);
     const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
     });
-*/
+
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
 }
 
-exports.connect = connect;
+//connect();
+
+// Consulta o banco de dados
+
+async function consulta(instrucao){
+    const conn = await connect();
+    const [rows] = await conn.query(instrucao);
+    return rows;
+}
+
+// Insere Dados
+
+async function adiciona(instrucao, valores){
+    const conn = await connect();
+//    const sql = 'INSERT INTO clientes(nome,idade,uf) VALUES (?,?,?);';
+//    const values = [customer.nome, customer.idade, customer.uf];
+    return await conn.query(instrucao, valores);
+}
+
+// Atualiza Dados
+
+async function atualiza(instrucao, valores){
+    const conn = await connect();
+//    const sql = 'UPDATE clientes SET nome=?, idade=?, uf=? WHERE id=?';
+//    const values = [customer.nome, customer.idade, customer.uf, id];
+    return await conn.query(instrucao, valores);
+}
+
+// Apaga Registro
+
+async function apaga(instrucao){
+    const conn = await connect();
+//    const sql = 'DELETE FROM clientes where id=?;';
+    return await conn.query(instrucao);
+}
+
+// Seção
+
+
+
+module.exports = {connect, consulta, adiciona, atualiza, apaga};
--- server.js
+++ server.js
@@ -2,8 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const db = require('./db');
-db.connect();
+
+/*
+(async () => {
+    let instrucao = 'SELECT id,observacoes FROM ct_artigos'
+    const clientes = await db.consulta(instrucao);
+    console.log(clientes);
+})();
+*/
+
+//db.connect();
 const port = process.env.PORT;
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
 const helmet = require('helmet');
@@ -11,10 +23,26 @@ const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
 const { connect } = require('http2');
+const { Store } = require('express-session');
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: process.env.SESS_SECRET,
+    Store: new MySQLStore(db.connect()),
+    connectionLimit: 10,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    },
+    createDatabaseTable: false
+});
+app.use(sessionOptions);
+app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
